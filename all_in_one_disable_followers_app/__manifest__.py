c71be3e4b2369044a831c88b7e1f299ba3b99e27
Akash Patel <akashpatel@browseinfo.in>
2021-05-28 10:54:15 +0530

    [REMOVE] Remove unused dependency for all_in_one_disable_followers_app for v14

--- all_in_one_disable_followers_app/__manifest__.py
+++ all_in_one_disable_followers_app/__manifest__.py
@@ -6,10 +6,10 @@
     'version' : '14.0.1.0',
     'live_test_url':'https://youtu.be/jMmauSrwRvY',
     "images":['static/description/main_screenshot.png'],
-    'summary' : 'All Disable follower on Odoo Disable follower on sale disable follower on purchase disable follower on invoice disable follower on bill disable follower stop subscription stop auto follower stop automatic follower stop follower on order disable follower ',
+    'summary' : 'All Disable follower on Odoo Disable follower on sale disable follower on invoice disable follower on bill disable follower stop subscription stop auto follower stop automatic follower stop follower on order disable follower ',
     'description' : """ Odoo automatically partners customers, vendors, contacts added as the followers. This module disables the partners automatically added as followers.  """,
     "license" : "OPL-1",
-    'depends' : ['base','sale_management','account','stock','purchase'],
+    'depends' : ['sale_management','account'],
     'data' : [
         'wizard/res_config_setting_views.xml',
     ],
